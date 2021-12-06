718fc0740f9fb3d589d67cdfeea034a6a92c2224
Mohamed Al Qesi <you@example.com>
2021-12-06 20:21:24 +0200

    added changeable name and age

--- src/state.js
+++ src/state.js
@@ -4,24 +4,41 @@ class State extends Component {
     constructor(){
         super()
         this.state = {
-            value : 0
+            value : 0,
+            name: null,
+            age: null            
         }
 
     this.plusClick = this.plusClick.bind(this)
     this.subtractClick = this.subtractClick.bind(this)
     this.clearClick = this.clearClick.bind(this)
+    this.change = this.change.bind(this)
+
+
      
     }
     
     plusClick (){
         this.setState({ value: this.state.value + 1 })
-    }
+        alert("صفا طلال حسن احمد محمد القيسي (غبية)")
+    };
     subtractClick (){
         this.setState({ value: this.state.value -  1 })
-    }
+    };
     clearClick (){
         this.setState({ value: 0 })
-    }
+    };
+    change(event) {
+
+        let id = event.target.id
+
+        this.setState({ [id]: event.target.value })
+    };
+
+/*    changeage(event) {
+        this.setState({ age: event.target.value })
+    };*/
+
     render(){
         return(
             <div>
@@ -29,6 +46,13 @@ class State extends Component {
                 <button onClick={this.plusClick} >Click To Add</button>
                 <button onClick={this.subtractClick} >Click to subtract</button>
                 <button onClick={this.clearClick} >Click to clear</button>
+
+                <h1>Your name is: {this.state.name}</h1>
+                <h1>You are: {this.state.age} years old</h1>
+
+                <input type="text" id = "name" onChange = {this.change} placeholder="Yourname" />
+
+                <input type="number" id="age" onChange = {this.change} placeholder="Yourage" />
             </div>
         )
     }
